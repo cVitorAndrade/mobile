df3bbdf4844958e6f2e662a457afd56fd427b41d
cVitorAndrade <vitorandrade1804@gmail.com>
2025-01-10 22:00:52 -0300

    feat: add alarmApp

---
+++ AlarmApp/app/src/main/java/com/example/alarmapp/ui/theme/Color.kt
@@ -0,0 +1,11 @@
+package com.example.alarmapp.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Green80 = Color(0xFFA8E6A1)  // Verde claro
+val GreenGrey80 = Color(0xFF76A56D)  // Verde cinza claro
+val Lime80 = Color(0xFFF0E68C)  // Verde limão claro
+
+val Green40 = Color(0xFF4CAF50)  // Verde claro mais forte
+val GreenGrey40 = Color(0xFF607D3B)  // Verde cinza
+val Lime40 = Color(0xFFCDFF6A)  // Verde limão mais forte
